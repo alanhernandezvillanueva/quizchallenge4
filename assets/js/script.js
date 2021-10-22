5fbbd6bbf8b1b88e126e4f2f558035c23b934ae5
alanhernandezvillanueva <79774478+alanhernandezvillanueva@users.noreply.github.com>
2021-10-21 22:28:13 -0700

    added capability to distinguish between correct and wrong answers, added timer and score page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
+//variables declared for later use 
 var startGame = document.querySelector("#start-game");
 var questionContainerEl = document.querySelector("#question-info");
 var questionText = document.querySelector("#question-text");
 var buttonDivEl = document.querySelector("#button-div");
 var correctAnswer = document.querySelector(".question-wrapper")
-
-
-
+var timerEl = document.getElementById('timer');
+var timerLeft = 50;
+var finalScoreEl = document.getElementById("final-score");
+ timerEl.innerHTML = timerLeft;
+ var score = 0;
 
 
   
 
-
+// questions object 
 var questionsArray = [
     {
         question: "What is the US Capital?",
@@ -38,16 +41,32 @@ var questionsArray = [
     }
 
 ];
-
+//variables use for the timer and goiing through all questions 
 var round = 0;
+var timerInterval;
 
+
+//start game function that will be triggered once the start quizz button is clicked 
 startGame.addEventListener("click", function(){
+     timerInterval = setInterval(function(){
+    //timer goes here 
+    if (timerLeft > 0 ) {
+        timerLeft--; 
+         timerEl.innerHTML = "You Have Seconds " + timerLeft;
+        } else{
+            endOfQuizz();
+            
+                }
+            }, 
+    
+    1000);
+
+    //create questions and answer button dynamically 
     buttonDivEl.innerHTML = "";
     questionText.innerHTML = questionsArray[round].question;
     questionContainerEl.innerHTML = "";
     for (var i =0; i < questionsArray[round].answerChoices.length; i++){
-       
-        var buttonEl = document.createElement("button")
+       var buttonEl = document.createElement("button")
         buttonEl.addEventListener("click", buttonListener)
         buttonEl.innerHTML = questionsArray[round].answerChoices[i]; 
         questionContainerEl.append(buttonEl);
@@ -56,14 +75,17 @@ startGame.addEventListener("click", function(){
 
 });
 
-
+//check if answers are right or wrong 
 var buttonListener = function(event) {
 
     if (event.target.textContent === questionsArray[round].answer) {
-        window.alert("Correct!");
+        window.alert("Correct!")
+     ;
         
     } else {
        window.alert("wrong");
+       //score goes down
+       timerLeft - 10;
     }
 
     round++;
@@ -94,29 +116,51 @@ var buttonListener = function(event) {
 
 };
 
-var scorePoints = function(){
-    //create a condition that awards points for every correct answer
-    
-    }
 
 
 
 
 
+// end quizz by displaying final score and ask player for their initials 
 var endOfQuizz = function() {
-    window.alert("The Quizz Is Finished, Let See How You Did");
-
-    var newScore = function() {
-
-    }
   
+  
+    clearInterval(timerInterval);
+    timerEl.innerHTML = "";
+    questionText.innerHTML = "";
+    questionContainerEl.innerHTML = "";
+       var score = timerLeft;
+       var scoreText =  document.createElement("h2");
+       scoreText.textContent = ("Your Score Is " + score);
+       finalScoreEl.append(scoreText);
+
+       var playerInitialsInput = document.createElement("input");
+       playerInitialsInput.setAttribute("placeholder", "Enter Your Initials Here");
+       finalScoreEl.append(playerInitialsInput);
+
+       var submitInitialsBtn = document.createElement("button");
+       submitInitialsBtn.textContent = ("Save");
+       finalScoreEl.append(submitInitialsBtn);
+
+
+
+
+
+    // window.alert("The Quizz Is Finished, Let See How You Did");
+    // //stop the timer and take the timer value and added to the score 
+    // score = score + timerLeft;
+    // // innerHTML to be the score input box for initials  and submit button 
+
+    // // when they push submit then get inpit box value and the score and set to local host.set 
    
-    var newHighScore = localStorage.getItem("newScore");
-    if(newHighScore === null){
-      newHighScore = 0;
-    } else {
-        window.prompt("We Have a New Record")
-    }
+    // var newHighScore = localStorage.getItem("newScore");
+    // if(newHighScore === null){
+    //   newHighScore = 0;
+    // } else {
+    //     window.prompt("We Have a New Record")
+    // }
   
 };
   
+
+
